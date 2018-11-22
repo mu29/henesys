7007e1cec1e94a494a97fa5347d54217fcc7f39e
InJung Chung <mu29@yeoubi.net>
2018-11-22 13:45:45 +0900

    fix: 미션 화면 우측에 스왑 버튼을 표시한다

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -4,23 +4,30 @@ import {
   StyleSheet,
 } from 'react-native'
 import { Text } from 'src/components'
-import { typography } from 'src/styles'
+import { typography, palette } from 'src/styles'
 
 const styles = StyleSheet.create({
   container: {
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+  },
+  border: {
+    borderBottomColor: palette.gray['30'],
+    borderBottomWidth: 1,
   },
 })
 
 export interface HeaderProps {
   title: string,
-  children?: Node,
+  border?: boolean,
+  children?: React.ReactElement<any>,
 }
 
-const Header: React.SFC<HeaderProps> = ({ title, children }) => (
-  <View style={styles.container}>
+const Header: React.SFC<HeaderProps> = ({ title, border, children }) => (
+  <View style={[styles.container, border && styles.border]}>
     <Text style={typography.heading['1'].black}>
       {title}
     </Text>
--- src/screens/MissionScreen.tsx
+++ src/screens/MissionScreen.tsx
@@ -2,12 +2,18 @@ import React from 'react'
 import { SafeAreaView } from 'react-navigation'
 import { StyleSheet } from 'react-native'
 import {
+  Button,
+  Icon,
   Header,
 } from 'src/components'
+import { palette } from 'src/styles'
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  swapButton: {
+    padding: 4,
   },
 })
 
@@ -15,7 +21,11 @@ class MissionScreen extends React.PureComponent {
   render() {
     return (
       <SafeAreaView style={styles.container}>
-        <Header title="별빛뒤로" />
+        <Header title="친구">
+          <Button style={styles.swapButton} onPress={() => {}}>
+            <Icon name="md-swap" size={20} color={palette.gray['90']} />
+          </Button>
+        </Header>
       </SafeAreaView>
     )
   }
