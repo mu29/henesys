8b7c5207ff42d12d17e3ccc22b7be372deac509b
InJung Chung <mu29@yeoubi.net>
2018-11-22 15:56:41 +0900

    feat: Divider 컴포넌트를 추가한다

---
+++ src/components/base/Divider.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import {
+  View,
+  StyleSheet,
+} from 'react-native'
+import { palette } from 'src/styles'
+
+const styles = StyleSheet.create({
+  vertical: {
+    width: 1,
+    height: '100%',
+    marginHorizontal: 4,
+  },
+  horizontal: {
+    width: '100%',
+    height: 1,
+    marginHorizontal: 4,
+  },
+})
+
+export interface DividerProps {
+  vertical?: boolean,
+  color?: string,
+}
+
+const Divider: React.SFC<DividerProps> = ({
+  vertical,
+  color,
+}) => {
+  const withColor = { backgroundColor: color }
+  return (
+    <View style={[vertical ? styles.vertical : styles.horizontal, withColor]} />
+  )
+}
+
+Divider.defaultProps = {
+  color: palette.gray['20'],
+}
+
+export default React.memo(Divider)
