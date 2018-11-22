143dab0180aede1e396dcb92abd0583cd692a2b0
InJung Chung <mu29@yeoubi.net>
2018-11-22 15:57:36 +0900

    feat: react-native-progress 라이브러리를 추가한다

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,13 @@
+import * as Progress from 'react-native-progress'
+export { Progress }
 export { default as Icon } from 'react-native-vector-icons/Ionicons'
 
 export { default as Button } from './base/Button'
+export { default as Divider } from './base/Divider'
 export { default as Text } from './base/Text'
 
 export { default as Header } from './common/Header'
 
-export { default as Avatar } from './user/Avatar'
+export { default as MissionList } from './mission/MissionList'
+
+export { default as UserInfo } from './user/UserInfo'
