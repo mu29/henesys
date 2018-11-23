a5b79ac5aadfd9aa919ab30ebcdf24c1d0489a95
InJung Chung <mu29@yeoubi.net>
2018-11-23 17:34:30 +0900

    feat: moment 라이브러리를 추가한다

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import { YellowBox } from 'react-native'
 import { Provider } from 'react-redux'
+import moment from 'moment'
+// @ts-ignore
+import momentLocaleKorea from 'moment/locale/ko'
 import { api } from 'src/services/api'
 import { parser } from 'src/services/parser'
 import configureStore from 'src/store/configure'
 import Navigator from 'src/Navigator'
+
+moment.updateLocale('ko', momentLocaleKorea)
 
 YellowBox.ignoreWarnings(['relay:check'])
 
