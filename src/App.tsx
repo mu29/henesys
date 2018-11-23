f2d633d087b2976bffd16cc36dbc87c096eb5f68
InJung Chung <mu29@yeoubi.net>
2018-11-23 15:03:19 +0900

    pref: React Native Debugger 연결 시 경고를 무시한다

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import { YellowBox } from 'react-native'
 import { Provider } from 'react-redux'
 import { api } from 'src/services/api'
 import { parser } from 'src/services/parser'
 import configureStore from 'src/store/configure'
 import Navigator from 'src/Navigator'
 
+YellowBox.ignoreWarnings(['relay:check'])
+
+// @ts-ignore
 const store = configureStore({}, { api, parser })
 
 export default () => (
