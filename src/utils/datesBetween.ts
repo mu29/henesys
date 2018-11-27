2b7218128f739e3de479d5ef21f60092eb4dd76f
InJung Chung <mu29@yeoubi.net>
2018-11-27 14:33:32 +0900

    feat: datesBetween 유틸리티를 추가한다

---
+++ src/utils/datesBetween.ts
@@ -0,0 +1,13 @@
+import { Moment } from 'moment'
+
+export default (start: Moment, end: Moment) => {
+  const dates = []
+  const current = start.startOf('day')
+  const last = end.startOf('day')
+
+  while (current.add(1, 'days').diff(last) < 0) {
+    dates.push(current.clone().format('YYYY-MM-DD'))
+  }
+
+  return dates
+}
