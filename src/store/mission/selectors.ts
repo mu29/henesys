eed3d4d68a048bd38a4b7a8f012e8354adf1abd3
InJung Chung <mu29@yeoubi.net>
2018-11-27 16:05:44 +0900

    fix: MissionState 의 todo 에 모든 미션을 넣어준다

--- src/store/mission/selectors.ts
+++ src/store/mission/selectors.ts
@@ -1,10 +1,12 @@
+import { missions } from 'src/constants/missions'
+
 export type MissionState = {
   todos: string[];
   records: { [key: string]: { [key: string]: boolean } };
 }
 
 const initialState: MissionState = {
-  todos: [],
+  todos: Object.values(missions).reduce((r: string[], c) => [...r, ...c.items.map(i => i.key)], []),
   records: {},
 }
 
