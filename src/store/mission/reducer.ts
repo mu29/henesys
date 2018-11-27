812f1977f38e2b780917862806d7cf902d25cc35
InJung Chung <mu29@yeoubi.net>
2018-11-27 16:15:41 +0900

    fix: 할 일 목록을 추가 / 제거할 때도 기록을 수정한다

--- src/store/mission/reducer.ts
+++ src/store/mission/reducer.ts
@@ -12,16 +12,31 @@ import { datesBetween, today } from 'src/utils'
 
 export default (state: MissionState = initialState, action: Action): MissionState => {
   if (isType(action, addTodoAction)) {
+    const now = today()
     return {
       ...state,
       todos: [...state.todos, action.payload.name],
+      records: {
+        ...state.records,
+        [now]: {
+          ...state.records[now],
+          [action.payload.name]: false,
+        },
+      },
     }
   }
 
   if (isType(action, removeTodoAction)) {
+    const now = today()
+    const removedRecord = Object.assign({}, state.records[now])
+    delete removedRecord[action.payload.name]
     return {
       ...state,
       todos: state.todos.filter(todo => todo !== action.payload.name),
+      records: {
+        ...state.records,
+        [now]: removedRecord,
+      },
     }
   }
 
