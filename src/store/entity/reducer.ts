a78b9b843faa7399f0cacae57bb60d2f8b9f6dec
InJung Chung <mu29@yeoubi.net>
2018-11-28 18:06:38 +0900

    fix: entity 리듀서가 제대로 작동하지 않던 오류를 수정한다

--- src/store/entity/reducer.ts
+++ src/store/entity/reducer.ts
@@ -5,8 +5,8 @@ import initialState, { EntityState } from './selectors'
 
 export default (state: EntityState = initialState, action: AnyAction): EntityState => {
   const { payload, meta } = action
-  if (action.type && action.type.endsWith('_DONE') && meta && meta.schema && payload) {
-    const { entities } = normalize(payload, Array.isArray(payload) ? [meta.schema] : meta.schema)
+  if (action.type && action.type.endsWith('_SUCCESS') && meta && meta.schema && payload) {
+    const { entities } = normalize(payload.result, Array.isArray(payload.result) ? [meta.schema] : meta.schema)
     return mergeWith({}, state, entities)
   }
 
