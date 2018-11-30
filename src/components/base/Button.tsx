e6988032c49c5ff078f6ad37bb88a2fd42948b82
InJung Chung <mu29@yeoubi.net>
2018-11-30 10:51:26 +0900

    fix: TouchableNativeFeedback 을 사용하는 Button 의 자식 컴포넌트를 View 로 확실히 감싸도록 수정한다

--- src/components/base/Button.tsx
+++ src/components/base/Button.tsx
@@ -1,37 +1,52 @@
 import React from 'react'
 import {
+  View,
   TouchableOpacity,
   TouchableNativeFeedback,
   Platform,
   TouchableOpacityProps,
   TouchableNativeFeedbackProps,
+  StyleProp,
+  ViewStyle,
 } from 'react-native'
 
 export interface ButtonProps extends TouchableOpacityProps, TouchableNativeFeedbackProps {
   onPress: () => void,
+  round?: boolean,
+  style?: StyleProp<ViewStyle>,
   children: React.ReactElement<any>,
 }
 
-const Button: React.SFC<ButtonProps> = ({
+const Button: React.FunctionComponent<ButtonProps> = ({
   onPress,
+  round,
+  style,
   children,
   ...props
-}) => Platform.OS === 'ios' ? (
-  <TouchableOpacity
-    activeOpacity={0.8}
-    onPress={onPress}
-    {...props}
-  >
-    {children}
-  </TouchableOpacity>
-) : (
-  <TouchableNativeFeedback
-    onPress={onPress}
-    background={TouchableNativeFeedback.SelectableBackground()}
-    {...props}
-  >
-    {children}
-  </TouchableNativeFeedback>
-)
+}) => {
+  const wrappedByView = typeof children.type !== 'string' && children.type.displayName === 'View'
+  return Platform.OS === 'ios' ? (
+    <TouchableOpacity
+      activeOpacity={0.8}
+      onPress={onPress}
+      style={style}
+      {...props}
+    >
+      {children}
+    </TouchableOpacity>
+  ) : (
+    <TouchableNativeFeedback
+      onPress={onPress}
+      background={round
+        ? TouchableNativeFeedback.SelectableBackgroundBorderless()
+        : TouchableNativeFeedback.SelectableBackground()
+      }
+      style={wrappedByView && style}
+      {...props}
+    >
+      {wrappedByView ? children : (<View style={style}>{children}</View>)}
+    </TouchableNativeFeedback>
+  )
+}
 
 export default React.memo(Button)
