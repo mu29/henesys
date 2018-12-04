8b9d9fc22e89986a95c43826dc0e989971b3b594
InJung Chung <mu29@yeoubi.net>
2018-12-04 15:36:06 +0900

    fix: 캐릭터 정보를 캐릭터 목록 페이지에서 파싱하도록 수정한다

--- src/services/parser.ts
+++ src/services/parser.ts
@@ -1,15 +1,16 @@
 import cheerio from 'react-native-cheerio'
 
-const CHARACTER_INFO_URL = 'https://maplestory.nexon.com/Common/Character/Detail'
-
+const CHARACTER_INFO_URL = 'https://maplestory.nexon.com/Ranking/World'
 export class Parser {
   getCharacterInfo = async (name: string) => {
-    const $ = await this.parse(`${CHARACTER_INFO_URL}/${name}`)
+    const $ = await this.parse(`${CHARACTER_INFO_URL}?c=${name}`)
+    const search = $('.search_com_chk')
+    search.find('td').eq(2).find('br').replaceWith('\n')
     return {
       name,
-      job: $('.char_info').find('dd').eq(1).text().split('/')[1],
-      level: parseInt(($('.char_info').find('dd').eq(0).text().match(/\d+/) || ['0'])[0], 10),
-      imageUrl: $('.char_img').find('img').attr('src').replace('http:', 'https:').replace('/180/', '/'),
+      job: search.find('dd').eq(0).text().split('/')[1],
+      level: parseInt((search.find('td').eq(2).text().match(/\d+/) || ['0'])[0], 10),
+      imageUrl: search.find('.char_img').find('img').attr('src').replace('http:', 'https:').replace('/180/', '/'),
     }
   }
 
