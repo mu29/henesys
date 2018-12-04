0585ad4530b61f74b18f40ddf9a575f957e739ee
InJung Chung <mu29@yeoubi.net>
2018-12-04 18:11:17 +0900

    fix: 문의하기, 이용약관 설정 인터렉션을 추가한다

--- src/components/settings/SettingsItem.tsx
+++ src/components/settings/SettingsItem.tsx
@@ -6,22 +6,27 @@ import {
 import {
   Icon,
   Text,
+  Button,
   Divider,
 } from 'src/components'
 import { typography, palette } from 'src/styles'
 
 const styles = StyleSheet.create({
   container: {
+    height: 48,
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
-    paddingVertical: 12,
-    paddingHorizontal: 24,
+    paddingLeft: 24,
+    paddingRight: 16,
     backgroundColor: palette.white.default,
   },
   item: {
     flexDirection: 'row',
     alignItems: 'center',
+  },
+  icon: {
+    marginHorizontal: 8,
   },
   divider: {
     marginHorizontal: 16,
@@ -35,28 +40,48 @@ export interface SettingsItemProps {
   onPress?: () => void,
 }
 
-const SettingsItem: React.FunctionComponent<SettingsItemProps> = ({
-  title,
-  text,
-  last,
-  onPress,
-}) => (
-  <View>
-    <View style={styles.container}>
-      <Text style={typography.body[2].black}>
-        {title}
-      </Text>
-      <View style={styles.item}>
-        {text && (
-          <Text style={typography.body[2].lightGray}>
-            {text}
-          </Text>
-        )}
-        <Icon name="ios-arrow-forward" size={20} color={palette.gray[40]} />
+class SettingsItem extends React.PureComponent<SettingsItemProps> {
+  renderItem() {
+    const {
+      title,
+      text,
+      children,
+    } = this.props
+    return (
+      <View style={styles.container}>
+        <Text style={typography.body[2].black}>
+          {title}
+        </Text>
+        <View style={styles.item}>
+          {text && (
+            <Text style={typography.body[2].lightGray}>
+              {text}
+            </Text>
+          )}
+          { children || <Icon name="ios-arrow-forward" size={20} color={palette.gray[40]} style={styles.icon} /> }
+        </View>
       </View>
-    </View>
-    <Divider hidden={last} style={styles.divider} />
-  </View>
-)
+    )
+  }
+
+  renderInner() {
+    const { onPress } = this.props
+    return onPress ? (
+      <Button onPress={onPress}>
+        {this.renderItem()}
+      </Button>
+    ) : this.renderItem()
+  }
+
+  render() {
+    const { last } = this.props
+    return (
+      <View>
+        {this.renderInner()}
+        {!last && <Divider style={styles.divider} />}
+      </View>
+    )
+  }
+}
 
 export default SettingsItem
--- src/components/settings/SettingsList.tsx
+++ src/components/settings/SettingsList.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
 import {
   View,
+  Switch,
+  Linking,
   StyleSheet,
 } from 'react-native'
+import {
+  withNavigation,
+  NavigationInjectedProps,
+} from 'react-navigation'
 import {
   DividedScrollView,
   AccountInfo,
@@ -22,25 +28,43 @@ const styles = StyleSheet.create({
   },
 })
 
-export interface SettingsListProps {}
+export interface SettingsListProps extends NavigationInjectedProps {}
 
-const SettingsList: React.FunctionComponent<SettingsListProps> = () => (
-  <DividedScrollView contentContainerStyle={styles.contentContainer}>
-    <View style={styles.container}>
-      <AccountInfo />
-      <SettingsSection title="알림">
-        <SettingsItem title="할 일 완료 알림" />
-        <SettingsItem title="우르스 알림" last />
-      </SettingsSection>
-      <SettingsSection title="기타">
-        <SettingsItem title="문의하기" />
-        <SettingsItem title="이용약관" />
-        <SettingsItem title="오픈소스 라이센스" />
-        <SettingsItem title="앱 평가하기" />
-        <SettingsItem title="친구에게 추천하기" last />
-      </SettingsSection>
-    </View>
-  </DividedScrollView>
-)
+class SettingsList extends React.PureComponent<SettingsListProps> {
+  _openUrl = (url: string) => {
+    if (Linking.canOpenURL(url)) {
+      Linking.openURL(url)
+    }
+  }
 
-export default React.memo(SettingsList)
+  _openEmailClient = () => this._openUrl('mailto:mu29@yeoubi.net')
+
+  _openTermsPage = () => {
+    const { navigation } = this.props
+    navigation.push('WebView', { url: 'https://naver.com' })
+  }
+
+  render() {
+    return (
+      <DividedScrollView contentContainerStyle={styles.contentContainer}>
+        <View style={styles.container}>
+          <AccountInfo />
+          <SettingsSection title="알림">
+            <SettingsItem title="할 일 완료 알림" text="오후 9시" />
+            <SettingsItem title="우르스 알림" last>
+              <Switch />
+            </SettingsItem>
+          </SettingsSection>
+          <SettingsSection title="기타">
+            <SettingsItem title="문의하기" onPress={this._openEmailClient} />
+            <SettingsItem title="이용약관" onPress={this._openTermsPage} />
+            <SettingsItem title="오픈소스 라이센스" />
+            <SettingsItem title="친구에게 추천하기" last />
+          </SettingsSection>
+        </View>
+      </DividedScrollView>
+    )
+  }
+}
+
+export default withNavigation(SettingsList)
