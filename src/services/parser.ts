485f66c160c191ff18500fa92372e3c58e3f61d0
InJung Chung <mu29@yeoubi.net>
2019-01-18 18:07:59 +0900

    feat: 인벤 글 목록을 조회하는 getArticleList 함수를 추가한다

--- src/services/parser.ts
+++ src/services/parser.ts
@@ -1,11 +1,15 @@
 import cheerio from 'react-native-cheerio'
 
 const CHARACTER_INFO_URL = 'https://maplestory.nexon.com/Ranking/World'
+const INVEN_BASE_URL = 'http://m.inven.co.kr'
+
 export class Parser {
   getCharacterInfo = async (name: string) => {
     const $ = await this.parse(`${CHARACTER_INFO_URL}?c=${name}`)
     const search = $('.search_com_chk')
+
     search.find('td').eq(2).find('br').replaceWith('\n')
+
     return {
       name,
       job: search.find('dd').eq(0).text().split('/')[1],
@@ -13,6 +17,23 @@ export class Parser {
       imageUrl: search.find('.char_img').find('img').attr('src').replace('http:', 'https:').replace('/180/', '/'),
     }
   }
+
+  getArticleList = async (board: number, page: number) => {
+    const $ = await this.parse(`${INVEN_BASE_URL}/board/powerbbs.php?come_idx=${board}&p=${page}`)
+    const articles = $('li.articleSubject').not('.articleNotice')
+
+    return articles.map((_, article) => ({
+      id: Number($(article).find('.subject').eq(0).attr('href').replace(/.*&l=/, '')),
+      title: $(article).find('.title').eq(0).text().replace(/\[[가-힣]+\]/, '').trim(),
+      author: $(article).find('.writer').eq(0).text().replace(/Lv\.\d+/, '').trim(),
+      href: $(article).find('.subject').eq(0).attr('href'),
+      viewCount: Number($(article).find('.hit').eq(0).text().replace('조회:', '')),
+      commentCount: Number($(article).find('.cmtWrapForList').eq(0).text() || 0),
+      createdAt: new Date($(article).find('.postdate').find('span').attr('title')),
+    })).toArray()
+  }
+
+  getArticleInfo = async (id: number) => {}
 
   parse = (url: string) => fetch(url)
     .then(r => r.text())
