d24a1d2788f37dd55bb3435cd2b3611eb5198838
InJung Chung <mu29@yeoubi.net>
2019-01-22 23:30:55 +0900

    fix: withSafeArea 가 속성을 받을 수 있도록 수정한다

--- src/wrappers/withSafeArea.tsx
+++ src/wrappers/withSafeArea.tsx
@@ -3,7 +3,7 @@ import {
   StatusBar,
   StyleSheet,
 } from 'react-native'
-import { SafeAreaView } from 'react-navigation'
+import { SafeAreaView, SafeAreaViewProps } from 'react-navigation'
 import { palette } from 'src/styles'
 
 const styles = StyleSheet.create({
@@ -13,8 +13,17 @@ const styles = StyleSheet.create({
   },
 })
 
-const withSafeArea = (Component: React.ComponentType<any>) => (props: any) => (
-  <SafeAreaView style={styles.container} forceInset={{ top: 'always' }}>
+const withSafeArea = (
+  Component: React.ComponentType<any>,
+  safeAreaProps: SafeAreaViewProps = {},
+) => (
+  props: any,
+) => (
+  <SafeAreaView
+    forceInset={{ top: 'always' }}
+    style={styles.container}
+    {...safeAreaProps}
+  >
     <StatusBar backgroundColor={palette.white.default} barStyle="dark-content" />
     <Component {...props} />
   </SafeAreaView>
