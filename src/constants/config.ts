951a712ab16ba0f5696b93c5d7670c72988faaf4
InJung Chung <mu29@yeoubi.net>
2019-02-02 19:49:17 +0900

    fix: 애드몹 광고를 활성화한다

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -10,7 +10,7 @@ const config: {
   all: {
     env: process.env.NODE_ENV || 'development',
     apiUrl: 'http://localhost:3000',
-    admobEnabled: false,
+    admobEnabled: true,
   },
   production: {
     apiUrl: 'https://api.henesys.net',
